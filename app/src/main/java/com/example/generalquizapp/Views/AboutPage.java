844fd3db36357cdf74699699e46d8f4a5de473b5
shohpolat <39366434+shohpolat@users.noreply.github.com>
2021-07-22 21:53:22 +0500

    add new comment

--- app/src/main/java/com/example/generalquizapp/Views/AboutPage.java
+++ app/src/main/java/com/example/generalquizapp/Views/AboutPage.java
@@ -28,7 +28,6 @@ public class AboutPage extends AppCompatActivity {
             text.setText(R.string.about_eng);
             back.setText(R.string.about_back_eng);
         }else {
-            //gkengdlfkjbgfewnlkfse
             text.setText(R.string.about_uzb);
             back.setText(R.string.about_back_uzb);
         }
@@ -40,6 +39,6 @@ public class AboutPage extends AppCompatActivity {
             }
         });
 
-
+ //nebolshoy fiks
     }
 }
