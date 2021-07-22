824344341a781bb2ffaa833e70cb4e0c27eba4d8
shohpolat <39366434+shohpolat@users.noreply.github.com>
2021-07-22 20:23:58 +0500

    simple change in "About" page

--- app/src/main/java/com/example/generalquizapp/Views/AboutPage.java
+++ app/src/main/java/com/example/generalquizapp/Views/AboutPage.java
@@ -22,10 +22,13 @@ public class AboutPage extends AppCompatActivity {
 
         String LANGUAGE = getIntent().getStringExtra(CategoriesPage.EXTRA_LANG);
 
+
+
         if (LANGUAGE.equals("e")){
             text.setText(R.string.about_eng);
             back.setText(R.string.about_back_eng);
         }else {
+            //gkengdlfkjbgfewnlkfse
             text.setText(R.string.about_uzb);
             back.setText(R.string.about_back_uzb);
         }
